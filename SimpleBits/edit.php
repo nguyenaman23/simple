def807eb04c510551995d97be6a3d92093f5085d
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2021-10-18 20:48:48 +0700

    Add files via upload

---
+++ SimpleBits/edit.php
@@ -0,0 +1,6 @@
+<?php
+
+
+$agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36";
+
+$cookie = "_ga=GA1.2.1931228085.1634562716; _gid=GA1.2.750958682.1634562716; _gat=1; sbits_session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik4wVllkIiwibmFtZSI6Imtpcml0b2FzZmMiLCJyb2xlIjoiVXNlciIsImlhdCI6MTYzNDU2Mjc5NSwiZXhwIjoxNjM0OTk0Nzk1fQ.jpAC9ryap2DHjpfRuuFRNqLPStp5OGG6l1BoOpHkFaI; sbstc_ss=6109084; hideouttv=SIMPLE-N0VYd; bitmedia_fid=eyJmaWQiOiI4NjJkZDQwZWVhYzJhZDE2ZGRjMTgxZGE1MDM2MGM2NyIsImZpZG5vdWEiOiI5ODY2N2Y3YjllN2RiNTNkOTYxNzg0ZTMzY2U5YWRkZiJ9";
